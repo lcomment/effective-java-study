ad483f6c63ecbdc19f48e88d8258e332a66e7b27
lcomment <kohyunsuk98@gmail.com>
2023-03-28 08:34:19 +0900

    refac: change log

--- 00_실습코드/src/item1/TestServiceImpl.java
+++ 00_실습코드/src/item1/TestServiceImpl.java
@@ -2,8 +2,8 @@ package item1;
 
 public class TestServiceImpl implements TestService {
 
-    @Override
-    public void getName() {
-        System.out.println("정상 로드 되었습니다");
-    }
+   @Override
+   public void getName() {
+       System.out.println("Load Success");
+   }
 }
