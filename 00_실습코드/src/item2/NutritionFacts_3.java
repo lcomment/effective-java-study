4fb6e6819c39447ab83e685da26147baaf2e3713
lcomment <kohyunsuk98@gmail.com>
2023-03-30 08:52:21 +0900

    feat: create sample code about Builder Pattern

---
+++ 00_실습코드/src/item2/NutritionFacts_3.java
@@ -0,0 +1,57 @@
+package item2;
+
+public class NutritionFacts_3 {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // 필수 매개변수
+        private final int servingSize;
+        private final int servings;
+
+        // 선택 매개변수
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public NutritionFacts_3 build() {
+            return new NutritionFacts_3(this);
+        }
+
+        public Builder calories(int val) {
+            this.calories = val;
+            return this;
+        }
+        public Builder fat(int val) {
+            this.fat = val;
+            return this;
+        }
+        public Builder sodium(int val) {
+            this.sodium = val;
+            return this;
+        }
+        public Builder carbohydrate(int val) {
+            this.carbohydrate = val;
+            return this;
+        }
+    }
+
+    private NutritionFacts_3(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+}
